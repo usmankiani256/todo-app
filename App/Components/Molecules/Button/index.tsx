3846438c339687a168242ac9b5bedd8bed48db10
usmankiani256 <usmankiani256@gmail.com>
2021-12-23 13:51:27 +0500

    :sparkles: revamp the button component

--- App/Components/Molecules/Button/index.tsx
+++ App/Components/Molecules/Button/index.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react'
 import { Button as PaperButton } from 'react-native-paper'
+import useStyles from './styles'
 
 export type ButtonProps = React.ComponentProps<typeof PaperButton>
 
 const Button = (props: ButtonProps) => {
-  const { children, onPress, icon } = props
+  const { children, onPress, icon, ...rest } = props
+
+  const { button, buttonLabel } = useStyles()
 
   return (
-    <PaperButton {...props} icon={icon} mode="contained" onPress={onPress}>
+    <PaperButton
+      icon={icon}
+      mode="contained"
+      contentStyle={button}
+      labelStyle={buttonLabel}
+      onPress={onPress}
+      {...rest}>
       {children}
     </PaperButton>
   )
