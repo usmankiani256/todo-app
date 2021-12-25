7a7768b9f092054ffb5a272620e171e01ed3d526
usmankiani256 <usmankiani256@gmail.com>
2021-12-25 21:39:37 +0500

    :sparkles: update task item component

--- App/Components/Organisms/TaskItem/index.tsx
+++ App/Components/Organisms/TaskItem/index.tsx
@@ -5,17 +5,23 @@ import useStyles from './styles'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { Colors } from 'Theme'
 import { Text } from 'Components/Atoms'
+import { TaskDialog } from '@Molecules'
 
 export type TaskItemProps = {
   priority: 'high' | 'medium' | 'low'
   status: 'checked' | 'unchecked'
   description: string
   onToggle: () => void
-  onPress: () => void
 }
 
 const TaskItem = (props: TaskItemProps) => {
-  const { priority, status, description, onToggle, onPress } = props
+  const { priority, status, description, onToggle } = props
+
+  const [visible, setVisible] = React.useState(false)
+
+  const showDialog = () => setVisible(true)
+
+  const hideDialog = () => setVisible(false)
 
   let indicatorColor =
     priority === 'high'
@@ -29,37 +35,40 @@ const TaskItem = (props: TaskItemProps) => {
   const { container, indicator, text } = useStyles()
 
   return (
-    <TouchableRipple
-      testID="task-item-button"
-      onPress={onPress}
-      style={container}>
-      <>
-        <View
-          style={{
-            ...indicator,
-            backgroundColor: indicatorColor,
-            borderColor: indicatorColor,
-          }}
-        />
-        <Icon
-          testID="task-item-icon"
-          onPress={onToggle}
-          name={taskStatus ? 'checkbox-marked' : 'checkbox-blank-outline'}
-          color={taskStatus ? Colors.primary : Colors.tintGrey}
-          size={25}
-          style={{ opacity: taskStatus ? 0.7 : 1 }}
-        />
-        <Text
-          justify
-          style={{
-            ...text,
-            textDecorationLine: taskStatus ? 'line-through' : undefined,
-            opacity: taskStatus ? 0.7 : 1,
-          }}>
-          {description}
-        </Text>
-      </>
-    </TouchableRipple>
+    <>
+      <TouchableRipple
+        testID="task-item-button"
+        onPress={showDialog}
+        style={container}>
+        <>
+          <View
+            style={{
+              ...indicator,
+              backgroundColor: indicatorColor,
+              borderColor: indicatorColor,
+            }}
+          />
+          <Icon
+            testID="task-item-icon"
+            onPress={onToggle}
+            name={taskStatus ? 'checkbox-marked' : 'checkbox-blank-outline'}
+            color={taskStatus ? Colors.primary : Colors.tintGrey}
+            size={25}
+            style={{ opacity: taskStatus ? 0.7 : 1 }}
+          />
+          <Text
+            justify
+            style={{
+              ...text,
+              textDecorationLine: taskStatus ? 'line-through' : undefined,
+              opacity: taskStatus ? 0.7 : 1,
+            }}>
+            {description}
+          </Text>
+        </>
+      </TouchableRipple>
+      {visible && <TaskDialog edit {...props} hideDialog={hideDialog} />}
+    </>
   )
 }
 
