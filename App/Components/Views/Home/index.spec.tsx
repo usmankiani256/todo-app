079ce7c8a609be122f09db71b3124fe013a9ae15
usmankiani256 <usmankiani256@gmail.com>
2021-12-27 01:07:15 +0500

    :white_check_mark: update home screen tests

--- App/Components/Views/Home/index.spec.tsx
+++ App/Components/Views/Home/index.spec.tsx
@@ -2,26 +2,19 @@ import * as React from 'react'
 import { fireEvent, render } from '@testing-library/react-native'
 import HomeScreen from './index'
 
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from '@Redux'
+
 describe('HomeScreen Component', () => {
   it('renders correctly', () => {
     const props: any = {}
 
-    const rendered: any = render(<HomeScreen {...props} />).toJSON()
+    const rendered: any = render(
+      <ReduxProvider store={store}>
+        <HomeScreen {...props} />
+      </ReduxProvider>,
+    ).toJSON()
 
     expect(rendered).toMatchSnapshot()
   })
-
-  // it('navigates to SignIn screen', () => {
-  //   const props: any = {
-  //     navigation: {
-  //       navigate: jest.fn(),
-  //     },
-  //   }
-
-  //   const { getByTestId } = render(<HomeScreen {...props} />)
-
-  //   fireEvent.press(getByTestId('welcome-button-continue'))
-
-  //   expect(props.navigation.navigate).toBeCalledWith('SignIn')
-  // })
 })
