999d3060b10d6cd49464679a713c4da40dba4454
usmankiani256 <usmankiani256@gmail.com>
2021-12-26 16:15:44 +0500

    :sparkles: update the home screen flow

--- App/Components/Views/Home/service.tsx
+++ App/Components/Views/Home/service.tsx
@@ -1,12 +1,40 @@
 import {} from 'react-native'
 import { StackScreenProps } from '@Navigation/Stack/types'
+import React from 'react'
+import { updateTask } from '@Api'
+import { useDispatch, useSelector } from 'react-redux'
+import { getTasks } from 'Store/Redux/Tasks'
+import { RootState } from 'Store/Redux'
 
 const useService = (props: StackScreenProps) => {
-  function onContinue() {
-    props.navigation.navigate('SignIn')
+  const dispatch = useDispatch()
+
+  const [loading, setLoading] = React.useState(false)
+
+  const Tasks = useSelector((state: RootState) => state.Tasks)
+  const tasks = !Tasks.loading && Tasks.success && Tasks.data
+
+  React.useEffect(() => {
+    dispatch(getTasks())
+  }, [dispatch])
+
+  function onToggleStatus(id: number | any) {
+    if (tasks) {
+      setLoading(true)
+      let { status } = tasks.filter(t => t.id === id)[0]
+
+      updateTask(id, {
+        status: status === 'checked' ? 'unchecked' : 'checked',
+      }).then(() => {
+        setTimeout(() => {
+          dispatch(getTasks())
+          setLoading(false)
+        }, 500)
+      })
+    }
   }
 
-  return { onContinue }
+  return { tasks, loading, onToggleStatus }
 }
 
 export default useService
--- App/Components/Views/Home/ui.tsx
+++ App/Components/Views/Home/ui.tsx
@@ -3,55 +3,54 @@ import useService from './service'
 import useStyles from './styles'
 import { StackScreenProps } from '@Navigation/Stack/types'
 
-import { View } from 'react-native'
+import { View, ActivityIndicator } from 'react-native'
 import { Text } from '@Atoms'
 import { Search } from '@Molecules'
 import { Screen } from '@Templates'
 import { TaskItem, CreateTask } from '@Organisms'
+import { Task } from 'Api/Tasks'
 
 const Home = (props: StackScreenProps) => {
-  const { onContinue } = useService(props)
+  const { tasks, loading, onToggleStatus } = useService(props)
 
   const { container, heading, caption } = useStyles()
 
+  function renderTask(task: Task) {
+    const { id, description, status, priority } = task
+    return (
+      <TaskItem
+        id={id}
+        priority={priority}
+        status={status}
+        description={description}
+        onToggle={() => onToggleStatus(id)}
+      />
+    )
+  }
+
   return (
-    <Screen>
-      <View style={container}>
-        <Text w3 h3 style={heading}>
-          Hello User!
-        </Text>
-        <Text w5 p style={caption}>
-          You have X tasks
-        </Text>
-        <Search
-          // value={password}
-          // onChangeText={setPassword}
-          placeholder="Search Tasks"
-        />
-        <Text w2 h4 style={heading}>
-          Tasks:
-        </Text>
-        <TaskItem
-          priority="high"
-          status="checked"
-          onToggle={() => {}}
-          description="Create header page component"
-        />
-        <TaskItem
-          priority="medium"
-          status="unchecked"
-          onToggle={() => {}}
-          description="Meeting with developers"
-        />
-        <TaskItem
-          priority="low"
-          status="unchecked"
-          onToggle={() => {}}
-          description="Design UI prototype"
-        />
-        <CreateTask />
-      </View>
-    </Screen>
+    <>
+      <Screen>
+        <View style={container}>
+          <Text w3 h3 style={heading}>
+            Hello User!
+          </Text>
+          <Text w5 p style={caption}>
+            You have X tasks
+          </Text>
+          <Search
+            // value={password}
+            // onChangeText={setPassword}
+            placeholder="Search Tasks"
+          />
+          <Text w2 h4 style={heading}>
+            Tasks:
+          </Text>
+          {loading ? <ActivityIndicator /> : tasks && tasks.map(renderTask)}
+        </View>
+      </Screen>
+      <CreateTask />
+    </>
   )
 }
 
